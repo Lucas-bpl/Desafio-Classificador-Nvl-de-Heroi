787c730639077f918098e465cec129f13a7a29ed
Lucas Pita Lima <lucaspitalima@gmail.com>
2024-10-03 20:37:25 -0300

    Resolução Desafio

---
+++ script.js
@@ -0,0 +1,37 @@
+let heroi = "Stark";
+let xp = 12312312;
+
+
+if (xp < 1000){
+
+    console.log(`O herói ${heroi} está no rank Ferro!`)
+
+} else if (xp >= 1001, xp <= 2000) {
+
+    console.log(`O herói ${heroi} está no rank Bronze!`)
+
+} else if (xp >= 2001, xp <= 5000) {
+
+    console.log(`O herói ${heroi} está no rank Prata!`)
+    
+} else if (xp >= 5001, xp <= 7000) {
+
+    console.log(`O herói ${heroi} está no rank Ouro!`)
+    
+} else if (xp >= 7001, xp <= 8000) {
+
+    console.log(`O herói ${heroi} está no rank Platina!`)
+    
+} else if (xp >= 8001, xp <= 9000) {
+
+    console.log(` O herói ${heroi} está no rank Ascendente!`)
+    
+} else if (xp >= 9001, xp <= 10000) {
+
+    console.log(`O herói ${heroi} está no rank Imortal`)
+    
+} else  {
+
+    console.log(`Parabéns! O herói ${heroi} está no rank Radiante!`)
+    
+}
